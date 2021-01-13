bbcd05a51449da7afc044f95c37e41618e1d74c7
Iampato <858wpwaweru@gmail.com>
2021-01-13 14:49:23 +0300

    added post model

---
+++ lib/model/post_model.dart
@@ -0,0 +1,74 @@
+// To parse this JSON data, do
+//
+//     final post = postFromJson(jsonString);
+
+import 'dart:convert';
+
+Post postFromJson(String str) => Post.fromJson(json.decode(str));
+
+String postToJson(Post data) => json.encode(data.toJson());
+
+class Post {
+  Post({
+    this.status,
+    this.totalResults,
+    this.articles,
+  });
+
+  String status;
+  int totalResults;
+  List<Article> articles;
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+        status: json["status"],
+        totalResults: json["totalResults"],
+        articles: List<Article>.from(
+            json["articles"].map((x) => Article.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+        "totalResults": totalResults,
+        "articles": List<dynamic>.from(articles.map((x) => x.toJson())),
+      };
+}
+
+class Article {
+  Article({
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  DateTime publishedAt;
+  String content;
+
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+        author: json["author"],
+        title: json["title"],
+        description: json["description"],
+        url: json["url"],
+        urlToImage: json["urlToImage"],
+        publishedAt: DateTime.parse(json["publishedAt"]),
+        content: json["content"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "author": author ?? "",
+        "title": title ?? "",
+        "description": description ?? "",
+        "url": url ?? "",
+        "urlToImage": urlToImage ?? "",
+        "publishedAt": publishedAt.toIso8601String(),
+        "content": content ?? "",
+      };
+}
